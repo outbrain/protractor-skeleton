e48837e98a868d8f3247d611d9798f774b159f0c
tal <tal.aveksis@gmail.com>
2020-11-12 13:40:46 +0200

    Tal- Home Assignment

--- tests/google.spec.ts
+++ tests/google.spec.ts
@@ -1,10 +1,27 @@
 import { wikiHomePage  } from '../page-objects/wikiHome.po'
+import {$, browser, ElementFinder} from 'protractor';
 
 describe('Wikipedia Search',  () => {
     it('should do a search', async ()  => {
       wikiHomePage.navigate();
-      wikiHomePage.typeSearch('we need you poster');
+	 ;
+	  
+	 // In the input field type a search value of your choice.
+	  var textSearch = 'python';
+      wikiHomePage.typeSearch(textSearch);
       wikiHomePage.clickSearch();
-      expect(true).toEqual(true);
+	  browser.sleep(1000);
+	  
+	 // Click on the third wiki result
+	  let thirdResult = $('#mw-content-text > div.mw-parser-output > ul:nth-child(7) > li:nth-child(1) > a');
+	  var txt = thirdResult.getText();
+      thirdResult.click();
+	  browser.sleep(1000);
+	  
+	  let resultTitle = $('#firstHeading');
+	  
+	 // Verify the title match the clicked link.
+	  expect(resultTitle.getText()).toContain(txt);
+
     });
   });
