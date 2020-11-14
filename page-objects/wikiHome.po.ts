c6c9644372ba955ca6aaa29af32925dd80acde29
tal <tal.aveksis@gmail.com>
2020-11-14 20:48:40 +0200

    1) Moving all selectors/locators to wikiHome.po. 2) Changing selectors/locators to be more generic. 3) Reformat the code. 4) Converting var to const. 5) Changing the name of the test file to be unique.

--- page-objects/wikiHome.po.ts
+++ page-objects/wikiHome.po.ts
@@ -1,12 +1,14 @@
-import {$, browser, ElementFinder} from 'protractor';
+import { $, browser, ElementFinder } from 'protractor';
 
 class WikiHomePage {
     private searchBox: ElementFinder;
-    private searchButton: ElementFinder;
+    private thirdSuggestions: ElementFinder;
+    private titleOfResultPage: ElementFinder;
 
     constructor() {
-        this.searchBox = $(`#searchInput`);
-        this.searchButton = $('#searchButton');
+        this.searchBox = $('#searchInput');
+        this.thirdSuggestions = $('body > div.suggestions > div > a:nth-child(3) > div');
+        this.titleOfResultPage = $('#firstHeading');
     }
 
     async navigate() {
@@ -17,9 +19,19 @@ class WikiHomePage {
         await this.searchBox.sendKeys(q);
     }
 
-    async clickSearch() {
-        await this.searchButton.click();
+    // click on the third result in wiki page
+    async clickOnTheSuggestion3() {
+        await this.thirdSuggestions.click();
     }
+    // get the title of the result page
+    async getTitleOfResultPage() {
+        await this.titleOfResultPage.getText();
+    }
+    // get the text of the third suggestion
+    async getTextOfSuggestion3() {
+        await this.thirdSuggestions.getText();
+    }
+
 }
 
 export const wikiHomePage = new WikiHomePage();
